ed1965842b9a7d1586f478c4baca550b4e2f78d5
Ian <i.r.butterworth@gmail.com>
2019-10-19 14:09:19 -0400

    build script

---
+++ deps/build.jl
@@ -0,0 +1,33 @@
+using Libdl
+
+function find_dwf()
+  if Sys.islinux()
+    path = "/usr/lib"
+    libdwf = "libdwf.so"
+  elseif Sys.isapple()
+    path = "/usr/local/lib"
+    libdwf = "dwf"
+  else
+    @error "DigilentWaveForms.jl only supported on Linux, and MacOS platforms"
+  end
+
+  return path, libdwf
+
+end
+
+function configure()
+
+  path, libdwf = find_dwf()
+  libdwf_C = find_library(libdwf,[path])
+
+  libdwf_C != "" || @error "Digilent WaveForm SDK cannot be found"
+
+  open(joinpath(@__DIR__, "deps.jl"), "w") do f
+    write(f, """
+      const libdwf_C = \"$(libdwf_C)\"
+    """)
+  end
+
+end
+
+configure()
