568cc41d4fdfe2e2b249210075e8c7cc664f84e1
Ian <i.r.butterworth@gmail.com>
2019-10-20 13:36:19 -0400

    try fix

--- src/enumeration.jl
+++ src/enumeration.jl
@@ -1,39 +1,39 @@
 export listdevices
 
 function listdevices()
-    cDevice = Cint(0)
-    cChannel = Cint(0)
-    hzFreq = Cdouble(0)
+    cDevice = Ref(Cint(0))
+    cChannel = Ref(Cint(0))
+    hzFreq = Ref(Cdouble(0))
     szDeviceName = zeros(Cchar, 32)
     szSN = zeros(Cchar, 32)
-    fIsInUse = Bool(0)
-    hdwf = HDWF(0)
+    fIsInUse = Ref(Bool(0))
+    hdwf = Ref(HDWF(0))
     szError = zeros(Cchar, 512)
 
     # detect connected all supported devices
-    if FDwfEnum(enumfilterAll, Ref(cDevice)) == 0
+    if FDwfEnum(enumfilterAll, cDevice) == 0
         FDwfGetLastErrorMsg(szError)
         error("FDwfEnum: $szError")
     end
     # list information about each device
-    @info "Found $cDevice devices:"
-    for i in 0:cDevice-1
+    @info "Found $(cDevice[]) devices:"
+    for i in 0:cDevice[]-1
         # we use 0 based indexing
-        FDwfEnumDeviceName(i, szDeviceName);
-        FDwfEnumSN(i, szSN);
-        println("Device: $(i+1) name: $szDeviceName $szSN")
+        FDwfEnumDeviceName(i, szDeviceName[]);
+        FDwfEnumSN(i, szSN[]);
+        println("Device: $(i+1) name: $(szDeviceName[]) $szSN")
         # before opening, check if the device isn�t already opened by other application, like: WaveForms
-        FDwfEnumDeviceIsOpened(i, Ref(fIsInUse));
+        FDwfEnumDeviceIsOpened(i, fIsInUse);
         if fIsInUse != 1
-            if FDwfDeviceOpen(i, Ref(hdwf)) != 1
+            if FDwfDeviceOpen(i, hdwf) != 1
                 FDwfGetLastErrorMsg(szError)
                 println("FDwfDeviceOpen: $szError")
                 break
             end
-            FDwfAnalogInChannelCount(hdwf, Ref(cChannel))
-            FDwfAnalogInFrequencyInfo(hdwf, C_NULL, Ref(hzFreq))
-            println("number of analog input channels: $cChannel maximum freq.: $hzFreq Hz")
-            FDwfDeviceClose(hdwf)
+            FDwfAnalogInChannelCount(hdwf, cChannel)
+            FDwfAnalogInFrequencyInfo(hdwf, C_NULL, hzFreq)
+            println("number of analog input channels: $(cChannel[]) maximum freq.: $(hzFreq[]) Hz")
+            FDwfDeviceClose(hdwf[])
             hdwf = hdwfNone
         end
     end
