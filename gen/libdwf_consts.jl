7c04d48b1013156112dc4283a735c5bde48cbb66
Ian <i.r.butterworth@gmail.com>
2019-10-20 11:26:10 -0400

    add const breaks and comments

--- gen/libdwf_consts.jl
+++ gen/libdwf_consts.jl
@@ -1,21 +1,25 @@
 # Consts wrapped by wrapconsts.jl
 
 const hdwfNone = HDWF(0)
+
 const enumfilterAll = ENUMFILTER(0)
 const enumfilterEExplorer = ENUMFILTER(1)
 const enumfilterDiscovery = ENUMFILTER(2)
 const enumfilterDiscovery2 = ENUMFILTER(3)
 const enumfilterDDiscovery = ENUMFILTER(4)
+
 const devidEExplorer = DEVID(1)
 const devidDiscovery = DEVID(2)
 const devidDiscovery2 = DEVID(3)
 const devidDDiscovery = DEVID(4)
+
 const devverEExplorerC = DEVVER(2)
 const devverEExplorerE = DEVVER(4)
 const devverEExplorerF = DEVVER(5)
 const devverDiscoveryA = DEVVER(1)
 const devverDiscoveryB = DEVVER(2)
 const devverDiscoveryC = DEVVER(3)
+
 const trigsrcNone = TRIGSRC(0)
 const trigsrcPC = TRIGSRC(1)
 const trigsrcDetectorAnalogIn = TRIGSRC(2)
@@ -33,6 +37,7 @@ const trigsrcExternal3 = TRIGSRC(13)
 const trigsrcExternal4 = TRIGSRC(14)
 const trigsrcHigh = TRIGSRC(15)
 const trigsrcLow = TRIGSRC(16)
+
 const DwfStateReady = DwfState(0)
 const DwfStateConfig = DwfState(4)
 const DwfStatePrefill = DwfState(5)
@@ -41,6 +46,7 @@ const DwfStateWait = DwfState(7)
 const DwfStateTriggered = DwfState(3)
 const DwfStateRunning = DwfState(3)
 const DwfStateDone = DwfState(2)
+
 const DECIAnalogInChannelCount = DwfEnumConfigInfo(1)
 const DECIAnalogOutChannelCount = DwfEnumConfigInfo(2)
 const DECIAnalogIOChannelCount = DwfEnumConfigInfo(3)
@@ -51,34 +57,41 @@ const DECIAnalogInBufferSize = DwfEnumConfigInfo(7)
 const DECIAnalogOutBufferSize = DwfEnumConfigInfo(8)
 const DECIDigitalInBufferSize = DwfEnumConfigInfo(9)
 const DECIDigitalOutBufferSize = DwfEnumConfigInfo(10)
+
 const acqmodeSingle = ACQMODE(0)
 const acqmodeScanShift = ACQMODE(1)
 const acqmodeScanScreen = ACQMODE(2)
 const acqmodeRecord = ACQMODE(3)
 const acqmodeOvers = ACQMODE(4)
 const acqmodeSingle1 = ACQMODE(5)
+
 const filterDecimate = FILTER(0)
 const filterAverage = FILTER(1)
 const filterMinMax = FILTER(2)
+
 const trigtypeEdge = TRIGTYPE(0)
 const trigtypePulse = TRIGTYPE(1)
 const trigtypeTransition = TRIGTYPE(2)
+
 const DwfTriggerSlopeRise = DwfTriggerSlope(0)
 const DwfTriggerSlopeFall = DwfTriggerSlope(1)
 const DwfTriggerSlopeEither = DwfTriggerSlope(2)
+
 const triglenLess = TRIGLEN(0)
 const triglenTimeout = TRIGLEN(1)
 const triglenMore = TRIGLEN(2)
-const dwfercNoErc = DWFERC(0)
-const dwfercUnknownError = DWFERC(1)
-const dwfercApiLockTimeout = DWFERC(2)
-const dwfercAlreadyOpened = DWFERC(3)
-const dwfercNotSupported = DWFERC(4)
-const dwfercInvalidParameter0 = DWFERC(0x10)
-const dwfercInvalidParameter1 = DWFERC(0x11)
-const dwfercInvalidParameter2 = DWFERC(0x12)
-const dwfercInvalidParameter3 = DWFERC(0x13)
-const dwfercInvalidParameter4 = DWFERC(0x14)
+
+const dwfercNoErc = DWFERC(0) #  No error occurred
+const dwfercUnknownError = DWFERC(1) #  API waiting on pending API timed out
+const dwfercApiLockTimeout = DWFERC(2) #  API waiting on pending API timed out
+const dwfercAlreadyOpened = DWFERC(3) #  Device already opened
+const dwfercNotSupported = DWFERC(4) #  Device not supported
+const dwfercInvalidParameter0 = DWFERC(0x10) #  Invalid parameter sent in API call
+const dwfercInvalidParameter1 = DWFERC(0x11) #  Invalid parameter sent in API call
+const dwfercInvalidParameter2 = DWFERC(0x12) #  Invalid parameter sent in API call
+const dwfercInvalidParameter3 = DWFERC(0x13) #  Invalid parameter sent in API call
+const dwfercInvalidParameter4 = DWFERC(0x14) #  Invalid parameter sent in API call
+
 const funcDC = FUNC(0)
 const funcSine = FUNC(1)
 const funcSquare = FUNC(2)
@@ -91,58 +104,71 @@ const funcTrapezium = FUNC(8)
 const funcSinePower = FUNC(9)
 const funcCustom = FUNC(30)
 const funcPlay = FUNC(31)
+
 const analogioEnable = ANALOGIO(1)
 const analogioVoltage = ANALOGIO(2)
 const analogioCurrent = ANALOGIO(3)
 const analogioPower = ANALOGIO(4)
 const analogioTemperature = ANALOGIO(5)
+
 const AnalogOutNodeCarrier = AnalogOutNode(0)
 const AnalogOutNodeFM = AnalogOutNode(1)
 const AnalogOutNodeAM = AnalogOutNode(2)
+
 const DwfAnalogOutModeVoltage = DwfAnalogOutMode(0)
 const DwfAnalogOutModeCurrent = DwfAnalogOutMode(1)
+
 const DwfAnalogOutIdleDisable = DwfAnalogOutIdle(0)
 const DwfAnalogOutIdleOffset = DwfAnalogOutIdle(1)
 const DwfAnalogOutIdleInitial = DwfAnalogOutIdle(2)
+
 const DwfDigitalInClockSourceInternal = DwfDigitalInClockSource(0)
 const DwfDigitalInClockSourceExternal = DwfDigitalInClockSource(1)
+
 const DwfDigitalInSampleModeSimple = DwfDigitalInSampleMode(0)
 const DwfDigitalInSampleModeNoise = DwfDigitalInSampleMode(1)
+
 const DwfDigitalOutOutputPushPull = DwfDigitalOutOutput(0)
 const DwfDigitalOutOutputOpenDrain = DwfDigitalOutOutput(1)
 const DwfDigitalOutOutputOpenSource = DwfDigitalOutOutput(2)
-const DwfDigitalOutOutputThreeState = DwfDigitalOutOutput(3)
+const DwfDigitalOutOutputThreeState = DwfDigitalOutOutput(3) # for custom and random
+
 const DwfDigitalOutTypePulse = DwfDigitalOutType(0)
 const DwfDigitalOutTypeCustom = DwfDigitalOutType(1)
 const DwfDigitalOutTypeRandom = DwfDigitalOutType(2)
 const DwfDigitalOutTypeROM = DwfDigitalOutType(3)
 const DwfDigitalOutTypeFSM = DwfDigitalOutType(3)
+
 const DwfDigitalOutIdleInit = DwfDigitalOutIdle(0)
 const DwfDigitalOutIdleLow = DwfDigitalOutIdle(1)
 const DwfDigitalOutIdleHigh = DwfDigitalOutIdle(2)
 const DwfDigitalOutIdleZet = DwfDigitalOutIdle(3)
-const DwfAnalogImpedanceImpedance = DwfAnalogImpedance(0)
-const DwfAnalogImpedanceImpedancePhase = DwfAnalogImpedance(1)
-const DwfAnalogImpedanceResistance = DwfAnalogImpedance(2)
-const DwfAnalogImpedanceReactance = DwfAnalogImpedance(3)
-const DwfAnalogImpedanceAdmittance = DwfAnalogImpedance(4)
-const DwfAnalogImpedanceAdmittancePhase = DwfAnalogImpedance(5)
-const DwfAnalogImpedanceConductance = DwfAnalogImpedance(6)
-const DwfAnalogImpedanceSusceptance = DwfAnalogImpedance(7)
-const DwfAnalogImpedanceSeriesCapactance = DwfAnalogImpedance(8)
-const DwfAnalogImpedanceParallelCapacitance = DwfAnalogImpedance(9)
-const DwfAnalogImpedanceSeriesInductance = DwfAnalogImpedance(10)
-const DwfAnalogImpedanceParallelInductance = DwfAnalogImpedance(11)
-const DwfAnalogImpedanceDissipation = DwfAnalogImpedance(12)
-const DwfAnalogImpedanceQuality = DwfAnalogImpedance(13)
-const DwfParamUsbPower = DwfParam(2)
-const DwfParamLedBrightness = DwfParam(3)
-const DwfParamOnClose = DwfParam(4)
-const DwfParamAudioOut = DwfParam(5)
-const DwfParamUsbLimit = DwfParam(6)
-const DwfParamKeepOnClose = DwfParam(1)
+
+const DwfAnalogImpedanceImpedance = DwfAnalogImpedance(0) # Ohms
+const DwfAnalogImpedanceImpedancePhase = DwfAnalogImpedance(1) # Radians
+const DwfAnalogImpedanceResistance = DwfAnalogImpedance(2) # Ohms
+const DwfAnalogImpedanceReactance = DwfAnalogImpedance(3) # Ohms
+const DwfAnalogImpedanceAdmittance = DwfAnalogImpedance(4) # Siemen
+const DwfAnalogImpedanceAdmittancePhase = DwfAnalogImpedance(5) # Radians
+const DwfAnalogImpedanceConductance = DwfAnalogImpedance(6) # Siemen
+const DwfAnalogImpedanceSusceptance = DwfAnalogImpedance(7) # Siemen
+const DwfAnalogImpedanceSeriesCapactance = DwfAnalogImpedance(8) # Farad
+const DwfAnalogImpedanceParallelCapacitance = DwfAnalogImpedance(9) # Farad
+const DwfAnalogImpedanceSeriesInductance = DwfAnalogImpedance(10) # Henry
+const DwfAnalogImpedanceParallelInductance = DwfAnalogImpedance(11) # Henry
+const DwfAnalogImpedanceDissipation = DwfAnalogImpedance(12) # factor
+const DwfAnalogImpedanceQuality = DwfAnalogImpedance(13) # factor
+
+const DwfParamUsbPower = DwfParam(2) # 1 keep the USB power enabled even when AUX is connected, Analog Discovery 2
+const DwfParamLedBrightness = DwfParam(3) # LED brightness 0 ... 100%, Digital Discovery
+const DwfParamOnClose = DwfParam(4) # 0 continue, 1 stop, 2 shutdown
+const DwfParamAudioOut = DwfParam(5) # 0 disable / 1 enable audio output, Analog Discovery 1, 2
+const DwfParamUsbLimit = DwfParam(6) # 0..1000 mA USB power limit, -1 no limit, Analog Discovery 1, 2
+const DwfParamKeepOnClose = DwfParam(1) # keep the device running after close, use DwfParamOnClose
+
 const trigcondRisingPositive = TRIGCOND(0)
 const trigcondFallingNegative = TRIGCOND(1)
+
 const stsRdy = STS(0)
 const stsArm = STS(1)
 const stsDone = STS(2)
--- gen/wrapconsts.jl
+++ gen/wrapconsts.jl
@@ -3,22 +3,29 @@ function wrapconsts(headerpath::String, outfile::String)
     filter!(x -> startswith(x, "const"), lines)
 
     juliaconsts = String[]
+    lasttype = ""
     for line in lines
         parts = split(line, r" +")
-
+        if occursin("//", line)
+            comment = string(" #",split(line, "//")[2])
+        else
+            comment = ""
+        end
         if length(parts) >= 4
             if parts[4] != "="
                 val = strip(parts[4], ';')
                 name = strip(parts[3], '=')
                 type = parts[2]
-                juliaconst = "const $(name) = $(type)($(val))"
+                juliaconst = "const $(name) = $(type)($(val))$comment"
             else
                 val = rstrip(parts[5], ';')
                 name = parts[3]
                 type = parts[2]
-                juliaconst = "const $(name) = $(type)($(val))"
+                juliaconst = "const $(name) = $(type)($(val))$comment"
             end
+            !(type == lasttype) && push!(juliaconsts, "")
             push!(juliaconsts, juliaconst)
+            lasttype = type
         else
             @warn "Couldn't parse $line"
         end
@@ -27,7 +34,7 @@ function wrapconsts(headerpath::String, outfile::String)
     isfile(outfile) && rm(outfile)
     @info "Writing outfile"
     open(outfile, "w") do io
-        write(io, "# Consts wrapped by wrapconsts.jl\n\n")
+        write(io, "# Consts wrapped by wrapconsts.jl\n")
         for juliaconst in juliaconsts
             write(io, juliaconst)
             write(io, "\n")
